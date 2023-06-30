d78fff615a5323eda107a744dfb076238fc48db5
eros.montin <eros.montin@gmail.com>
2023-06-30 08:09:07 -0400

    v2

--- cloudmrhub/test/testSNR_Grappa.py
+++ cloudmrhub/test/testSNR_Grappa.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cloudmrhub.cm2D as cm2D
-
+import cloudmrhub.cm as cm
 import matplotlib.pyplot as plt
 import  twixtools
-import cloudmrhub.cm as cm
+
 s='cloudmrhub/data/meas_MID00024_FID188178_Multislice.dat'
 n='cloudmrhub/data/meas_MID00027_FID188181_Multislice_no_RF.dat'
 
@@ -23,6 +23,7 @@ im_array.flags['average']['Ave'] = False # average all repetitions
 N=np.transpose(im_array[0,0,0,0,0,0,0,0,0,0,0,0,0,:,:,:],[2,0,1])
 
 
+
 FA=1
 PA=2
 ACLF=20
@@ -35,18 +36,39 @@ L.AccelerationP=PA
 L.AutocalibrationF=ACLF
 L.AutocalibrationP=ACLP
 US=cm.undersample2DDatamGRAPPA(S,frequencyacceleration=FA,phaseacceleration=PA,frequencyACL=ACLF,phaseACL=ACLP)
-
 L.setSignalKSpace(US)
 L.setNoiseKSpace(N)
 L.setGrappaKernel(GK)
 
 
-R=L.getR()
+L2=cm2D.cm2DSignalToNoiseRatioPseudoMultipleReplicas()
 
+L2.numberOfReplicas=100
+L2.reconstructor=L
 
-plt.figure()
-plt.imshow(np.abs(L.getOutput()))
+plt.subplot(121)
+plt.imshow(L2.getOutput(),vmax=250)
+plt.title('PMR')
 plt.colorbar()
-plt.title('Recon Grappa')
-plt.show()
 
+R2=cm2D.cm2DSignalToNoiseRatioPseudoMultipleReplicasWen()
+R=cm2D.cm2DReconRSS()
+R2.numberOfReplicas=5
+R2.boxSize=10
+R.setNoiseKSpace(N)
+R.setSignalKSpace(S)
+R2.reconstructor=R
+
+
+
+
+
+
+plt.subplot(122)
+plt.imshow(R2.getOutput(),vmax=250)
+plt.title('CR')
+plt.colorbar()
+
+
+
+plt.show()
---
+++ cloudmrhub/test/testrecon _grappa.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cloudmrhub.cm2D as cm2D
+
+import matplotlib.pyplot as plt
+import  twixtools
+import cloudmrhub.cm as cm
+s='cloudmrhub/data/meas_MID00024_FID188178_Multislice.dat'
+n='cloudmrhub/data/meas_MID00027_FID188181_Multislice_no_RF.dat'
+
+twix=twixtools.map_twix(s)
+im_array = twix[0]['image']
+im_array.flags['remove_os'] = True  # activate automatic os removal
+im_array.flags['average']['Rep'] = True  # average all repetitions
+im_array.flags['average']['Ave'] = True  # average all repetitions
+S=np.transpose(im_array[0,0,0,0,0,0,0,0,0,0,0,0,0,:,:,:],[2,0,1])
+
+
+twix=twixtools.map_twix(n)
+im_array = twix[0]['image']
+im_array.flags['remove_os'] = False  # activate automatic os removal
+im_array.flags['average']['Rep'] = False  # average all repetitions
+im_array.flags['average']['Ave'] = False # average all repetitions
+N=np.transpose(im_array[0,0,0,0,0,0,0,0,0,0,0,0,0,:,:,:],[2,0,1])
+
+
+FA=1
+PA=2
+ACLF=20
+ACLP=20
+GK=[3,2]
+
+L=cm2D.cm2DReconGrappa()
+L.AccelerationF=FA
+L.AccelerationP=PA
+L.AutocalibrationF=ACLF
+L.AutocalibrationP=ACLP
+US=cm.undersample2DDatamGRAPPA(S,frequencyacceleration=FA,phaseacceleration=PA,frequencyACL=ACLF,phaseACL=ACLP)
+
+L.setSignalKSpace(US)
+L.setNoiseKSpace(N)
+L.setGrappaKernel(GK)
+
+
+
+plt.figure()
+plt.imshow(np.abs(L.getOutput()))
+plt.colorbar()
+plt.title('Recon Grappa')
+plt.show()
+
